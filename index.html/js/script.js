92f97a7150dca9bc2dd3974d2fd3e88f6e058bf9
Clifford <stasser1@outlook.com>
2020-03-28 03:40:23 -0500

    removed some unneeded code

--- index.html/js/script.js
+++ index.html/js/script.js
@@ -1,10 +1,1 @@
 
-function initMap() {
-    var location = {lat: 28.820300, lng: -96.991620};
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: location
-    });
-  }
-
-  
